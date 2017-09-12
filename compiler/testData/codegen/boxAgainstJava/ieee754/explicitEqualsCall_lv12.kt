a059296b1da1ee9821c17ba94d485245c3525f34
Dmitry Petrov <dmitry.petrov@jetbrains.com>
2017-09-12 12:25:08 +0300

    Update IEEE 754 'equals' tests for LV 1.2
    NB IEEE 754 doesn't say anything about nulls.

--- compiler/testData/codegen/boxAgainstJava/ieee754/explicitEqualsCall_lv12.kt
+++ compiler/testData/codegen/boxAgainstJava/ieee754/explicitEqualsCall_lv12.kt
@@ -1,4 +1,4 @@
-// LANGUAGE_VERSION: 1.1
+// LANGUAGE_VERSION: 1.2
 // FILE: JavaClass.java
 
 public class JavaClass {
@@ -24,8 +24,6 @@ fun box(): String {
     val jClass = JavaClass()
 
     if (jClass.minus0().equals(jClass.plus0())) return "fail 5"
-
-    if (jClass.null0().equals(jClass.plus0())) return "fail 6"
     if (jClass.minus0().equals(jClass.null0())) return "fail 7"
     return "OK"
 }
